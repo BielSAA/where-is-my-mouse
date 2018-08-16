5b8276a849e8e3f89013173784f77fa9340fe6dc
Gabriel Pinheiro <bielrsgc@gmail.com>
2018-08-16 14:47:18 -0300

    Added arrow object that follows the mouse

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ $(() => {
 	const canvas = document.getElementById("mouse"),
 			 ctx = canvas.getContext("2d");
 
-	const BACKGROUND_COLOR = "#000"; // Color of the page background
+	const BACKGROUND_COLOR = "#000", // Color of the page background
+		  ARROW_DISTANCE   = 250,
+		  ARROW_SIZE       = 100,
+		  ARROW_SPEED      = 0.98,
+		  ARROW_ROTATION   = 0.02;
 
-	const mouse = new Vector2(-1e3, -1e3); // Starting mouse far from screen
+	const mouse = new Vector2(-1e3, -1e3), // Starting mouse far from screen
+		  arrow = new Arrow(null, new Vector2(ARROW_SIZE, 0)).setSpeed(ARROW_SPEED);  // Starting arrow
+
+	let rotation = 0; // Rotation of the arrow
 
 	/**
 	 * Draws a frame and adds the next one to the quere (requestAnimationFrame)
@@ -16,6 +23,17 @@ $(() => {
 			canvas.width = $(window).innerWidth();
 			canvas.height = $(window).innerHeight();
 		}
+
+		// Updating rotation
+		rotation = (rotation + ARROW_ROTATION) % (Math.PI * 2);
+
+		// Moving arrow
+		let rotationVector = new Vector2(Math.cos(rotation), Math.sin(rotation)) // Magnitude 1
+				.scale(ARROW_DISTANCE);                                          // Magnitude ARROW_DISTANCE
+		let targetPosition = mouse.clone().add(rotationVector);
+
+		arrow.move(targetPosition).point(mouse);
+		
 
 		// Clearing the canvas by filling it with the BACKGROUND_COLOR
 		ctx.fillStyle = BACKGROUND_COLOR;
